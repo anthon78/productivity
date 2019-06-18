c1bf494ce01c1c3c12b24c7cc8856d4528813423
anthon78 <economicsstudent2000@gmail.com>
2019-06-18 11:28:55 -0500

    complete addToAccepted db function

---
+++ server/database/db.js
@@ -0,0 +1,40 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const CONNECTION_URL = 'mongodb://localhost:27017/taskData';
+const DATABASE_NAME = 'taskData';
+
+module.exports.getAllAccepted = (callback) => {
+  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
+    if (error) {
+      callback(error);
+    } else {
+      database = client.db(DATABASE_NAME);
+      collection = database.collection('acceptedTasks');
+      collection.find({}).toArray((err,tasks) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null,tasks);
+        }
+      })
+    }
+  })
+}
+
+module.exports.addToAccepted = (task,callback) => {
+  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
+    if (error) {
+      callback(error);
+    } else {
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("acceptedTasks");
+      collection.insertOne(task, (err,result) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null,result);
+        }
+      })
+    }
+  })
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyparser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config()
 const app = express();
+const db = require('./database/db.js')
 const port = process.env.PORT;
 
 
@@ -14,7 +15,26 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/public/')));
 
 //routes
+app.get('/api/acceptedTasks', (req,res) => {
+  db.getAllAccepted((err,tasks) => {
+    if (err) {
+      console.log("Error retrieving accepted tasks", err);
+    } else {
+      res.send(tasks);
+    }
+  })
+})
 
+app.post('/api/acceptedTasks', (req,res) => {
+  let task = req.body;
+  db.addToAccepted(task,(err,tasks) => {
+    if (err) {
+      console.log("Error retrieving accepted tasks", err);
+    } else {
+      res.send(tasks);
+    }
+  })
+})
 
 //listen
 app.listen(port, console.log(`listening on port ${port}`))
