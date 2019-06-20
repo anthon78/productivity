ce72d6c32825b5482701ed99189ec78097f7a9ee
anthon78 <economicsstudent2000@gmail.com>
2019-06-19 19:35:00 -0500

    refactor db to only connect once

--- server/database/db.js
+++ server/database/db.js
@@ -4,95 +4,69 @@ const mongo = require("mongodb");
 //'mongodb://localhost:27017/taskData'
 const CONNECTION_URL = process.env.MONGO_URI;
 const DATABASE_NAME = 'taskData';
+let db;
+
+MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
+  if (error) {
+    console.log(error);
+  } else {
+    database = client.db(DATABASE_NAME);
+  }
+})
 
 module.exports.getAll = (callback) => {
-  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
-    if (error) {
-      callback(error);
+  collection = database.collection('acceptedTasks');
+  collection.find({}).toArray((err,tasks) => {
+    if (err) {
+      callback(err);
     } else {
-      database = client.db(DATABASE_NAME);
-      collection = database.collection('acceptedTasks');
-      collection.find({}).toArray((err,tasks) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null,tasks);
-        }
-      })
+      callback(null,tasks);
     }
   })
 }
 
 module.exports.addTask = (task,callback) => {
-  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
-    if (error) {
-      callback(error);
+  collection = database.collection("acceptedTasks");
+  collection.insertOne(task, (err,result) => {
+    if (err) {
+      callback(err);
     } else {
-      database = client.db(DATABASE_NAME);
-      collection = database.collection("acceptedTasks");
-      collection.insertOne(task, (err,result) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null,result);
-        }
-      })
+      callback(null,result);
     }
   })
 }
 
 module.exports.deleteTask = (description, callback) => {
-  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
-    if (error) {
-      callback(error);
+  collection = database.collection("acceptedTasks");
+  collection.deleteOne({description : description}, (err,result) => {
+    if (err) {
+      callback(err);
     } else {
-      database = client.db(DATABASE_NAME);
-      collection = database.collection("acceptedTasks");
-      collection.deleteOne({description : description}, (err,result) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null,result);
-        }
-      })
+      callback(null,result);
     }
   })
 }
 
 module.exports.getStats = (callback) => {
-  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error,client) => {
-    if (error) {
-      callback(error);
+  let o_id = new mongo.ObjectId("5d0a53121c9d4400004dba40");
+  collection = database.collection("taskStats");
+  collection.findOne({_id : o_id}, (err,result) => {
+    if (err) {
+      callback(err);
     } else {
-      let o_id = new mongo.ObjectId("5d0a53121c9d4400004dba40");
-      database = client.db(DATABASE_NAME);
-      collection = database.collection("taskStats");
-      collection.findOne({_id : o_id}, (err,result) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null,result);
-        }
-      })
+      callback(null,result);
     }
   })
 }
 
 module.exports.setStats = (stats, callback) => {
-  MongoClient.connect(CONNECTION_URL, {useNewURLParser: true}, (error, client) => {
-    if (error) {
-      callback(error);
+  let o_id = new mongo.ObjectId("5d0a53121c9d4400004dba40");
+  collection = database.collection("taskStats");
+  collection.updateOne({_id : o_id},{$inc: {xp:stats.xp, tasksCompleted: 1, rejects: stats.rejects}}, (err,result) => {
+    if (err) {
+      callback(err);
     } else {
-      let o_id = new mongo.ObjectId("5d0a53121c9d4400004dba40");
-      database = client.db(DATABASE_NAME);
-      collection = database.collection("taskStats");
-      collection.updateOne({_id : o_id},{$inc: {xp:stats.xp, tasksCompleted: 1, rejects: stats.rejects}}, (err,result) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null,result);
-        }
-      })
+      callback(null,result);
     }
   })
 }
